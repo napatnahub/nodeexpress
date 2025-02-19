6f3562ad12c436449dd3029ac2a37b86f7e1dd2c
napatnahub <napat.nahub@gmail.com>
2025-02-19 09:25:32 +0700

    update

--- SequlizeSQLiteCRUDBook.js
+++ SequlizeSQLiteCRUDBook.js
@@ -10,10 +10,70 @@ const sequelize = new Sequelize('database','username','password',{
     storage: './Database/SQBooks.sqlite'
 });
 
-const Book = sqequelize.define('book',{
+const Book = sequelize.define('book',{
     id:{
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-})
+    titlw:{
+        type: Sequelize.STRING,
+        allowNull : false
+    },
+    author:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+sequelize.sync();
+
+app.get('/books',(req,res)=>{
+    Book.findAll().then(books =>{
+        res.json(books);
+    }).catch(err=>{
+        res.status(500).send(err);
+    });
+});
+
+app.post('/books',(req,res)=>{
+    Book.create(req.body).then(book=>{
+        res.send(book);
+    }).catch(err =>{
+        res.status(500).send(err);
+    });
+});
+
+app.put('/books/:id',(req,res)=>{
+    Book.findByPk(req.params.id).then(book =>{
+        if (!book){
+            res.status(404).send('Book not found');
+        }else{
+            book.update(req.body).then(()=>{
+                res.send(book);
+            }).catch(err =>{
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err =>{
+        res.status(500).send(err);
+    });
+});
+
+app.delete('/books/:id',(req,res)=>{
+    Book.findByPk(req.params.id).then(book =>{
+        if (!book) {
+            res.status(404).send('Book not found');
+        }else{
+            book.destroy().then(()=>{
+                res.send({});
+            }).catch(err =>{
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err =>{
+        res.status(500).send(err);
+    });
+});
+const port = process.env.PORT||3000 ;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
